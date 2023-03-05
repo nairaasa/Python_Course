f72f92aabfa83bd2fc882e7b7afe59793016049a
nairaasa <asatryan.nairah@gmail.com>
2023-03-05 18:20:05 +0400

    Added Python_Workshop folder

---
+++ Python_Workshop/lesson1.py
@@ -0,0 +1,11 @@
+print("This is a script to test that Python is working...")
+
+print("Author: Naira Asatryan")
+print("My GitHub repo's link is here: https://github.com/nairaasa/Python_Course.git")
+print("How have I scheduled Python scripts with GitHub actions?")
+print("jobs: create a github repo")
+print("run these commands in VSCode: git config --global user.name 'nairaasa'")
+print("git config --global user.email 'asatriannara7gmail.com'")
+print("check git status by command: git status")
+
+print("And now Im going to have my changes to be committed by command: couldn't find the command, many attempts, but no result...")
