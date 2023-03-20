a4d07928c4f16a6579b5ffa8954775b8cc6706a5
nairaasa <asatryan.nairah@gmail.com>
2023-03-20 22:57:31 +0400

    modified:

--- Vic1/exercise2.py
+++ Vic1/exercise2.py
@@ -5,22 +5,10 @@ integer and returns wwhether it's a repdigit or not.
 """
 
 
-x = a = 4; b = 4;
-y = a / b == 1
+x = 44
+y = x // 10 == x % 10
 
 print(y)
 # True
 
 
-x = a = 4; b = 5;
-y = a / b == 1
-
-print(y)
-# False
-
-
-x = a = -4; b = 4;
-y = a / b == 1
-
-print(y)
-# False
